1fb533132eb051d046a2acbef572a5c6a0819d08
David Jiang <logiccubing@gmail.com>
2025-12-02 01:34:54 -0500

    m

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/scibowl-org/',
+  base: "/scibowl-org/",
   plugins: [react()],
-})
+});
