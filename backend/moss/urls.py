fbad0b40d47c4df91a3f5364c34b3d2c9abcaafa
dn285 <dn285@cornell.edu>
2026-03-21 23:51:31 -0500

    POST scoresheets endpoint

--- backend/moss/urls.py
+++ backend/moss/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import ScoresheetEventsView
+from .views import ScoresheetCreateView, ScoresheetEventsView
 
 urlpatterns = [
+    path("scoresheets/", ScoresheetCreateView.as_view(), name="scoresheet-create"),
     path("scoresheets/<int:scoresheet_id>/events/", ScoresheetEventsView.as_view(), name="scoresheet-events"),
 ]
